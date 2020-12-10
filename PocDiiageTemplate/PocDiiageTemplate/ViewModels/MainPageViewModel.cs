8d70473059ea8d76985a5c6531a1406fc4d5cee3
Thomas CARAYON <Thomas.CARAYON@diiage.org>
2020-12-10 13:29:11 +0100

    Reprise du projet donné, ajout de la navigation et du databinding

---
+++ PocDiiageTemplate/PocDiiageTemplate/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace PocDiiageTemplate.ViewModels
+{
+    using Prism.Commands;
+    using Prism.Navigation;
+
+    public class MainPageViewModel : ViewModelBase
+    {
+        private INavigationService _navigationService;
+        private DelegateCommand _delegateCommand;
+
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            _navigationService = navigationService;
+            Title = "Main Page";
+        }
+
+        public DelegateCommand buttonClick => _delegateCommand = new DelegateCommand(Button_Clicked);
+        private async void Button_Clicked()
+        {
+            await _navigationService.NavigateAsync("SecondPage");
+        }
+    }
+}
---
+++ PocDiiageTemplate/PocDiiageTemplate/ViewModels/SecondPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace PocDiiageTemplate.ViewModels
+{
+    using Prism.Navigation;
+
+    public class SecondPageViewModel : ViewModelBase
+    {
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set { SetProperty(ref _description, value); }
+        }
+        public SecondPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Second Page";
+            Description = "Ceci est une description personnalisée pour la seconde page !";
+        }
+    }
+}
